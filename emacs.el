761e8b4ee7efd97d65746ef19b296a789b72658a
COLDTURNIP <coldturnip@gmail.com>
2011-11-06 17:55:55 +0800

    Markdown mode: add file extension for *.markdown

--- emacs.el
+++ emacs.el
@@ -371,6 +371,7 @@
 (add-to-list 'auto-mode-alist '("\\.md" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.mdt" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.mdwn" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown" . markdown-mode))
 ;; avoid key conflict for tab with Yasnippets
 (defun markdown-unset-tab ()
   "markdown-mode-hook"
