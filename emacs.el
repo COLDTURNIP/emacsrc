b44ebe3747f16bc3bfd2ac7dbe3bd49186925d2e
COLDTURNIP <coldturnip@gmail.com>
2011-11-09 03:00:09 +0800

    Add plugin: Haskell Mode

--- emacs.el
+++ emacs.el
@@ -384,6 +384,16 @@
 ;;; }
 
 
+;;;; # Haskell Mode
+;;; {
+(load "~/.emacs.d/external/haskell-mode-2.8.0/haskell-site-file")
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
+;;; }
+
+
 ;;;; # Markdown mode #
 ;;; {
 (add-to-list 'load-path "~/.emacs.d/external/markdown-mode")
